b3345816e221134b50ab60a4f6a2bce2ae2c59de
unknown <Dragon8113>
2022-05-22 22:09:01 +0800

    完成

---
+++ 数的存储形式/双亲表示法/parent.c
@@ -0,0 +1,82 @@
+#include<stdio.h>
+
+#define ERROR 0
+#define OK 1
+#define FALSE 0
+#define TRUE 1
+#define MAX_TREE_SIZE 10  
+typedef int ElemType;
+typedef int Status;
+
+typedef struct{     //节点
+    ElemType data;  
+    int parent;     //双亲节点位置
+    /*  扩展用 
+    int Child1; //孩子节点位置
+    int Child2;
+    int Child3;
+    int RigSib; //兄弟节点位置
+     */
+} PTNode;
+
+typedef struct{     //树
+    PTNode Nodes[MAX_TREE_SIZE];
+    int r;  //根节点位置
+    int n;  //当前树的节点数目
+} PTree;
+
+/* 测试用的树
+            1
+    4      5      6
+  7  8       2   3   9   
+ */
+Status InitTree(PTree *tree){       //层次表示法: 1 4 5 6 7 8 2 3 9
+    ElemType data[] = { 1, 4, 5, 6, 7, 8, 2, 3, 9};
+    int length = sizeof(data)/ sizeof(data[0]);
+
+    for(int i = 0; i < length; i++){
+        tree->Nodes[i].data = data[i];
+    }
+    tree->r = 0;
+    tree->n = length;
+
+    tree->Nodes[0].parent = -1;
+    tree->Nodes[1].parent = 0;
+    tree->Nodes[2].parent = 0;
+    tree->Nodes[3].parent = 0;
+    tree->Nodes[4].parent = 1;
+    tree->Nodes[5].parent = 1;
+    tree->Nodes[6].parent = 2;
+    tree->Nodes[7].parent = 3;
+    tree->Nodes[8].parent = 3;
+    
+    return OK;
+}
+
+/* 测试用的树
+            1
+    4      5      6
+  7  8       2   3   9   
+ */
+int main(void){     //测试(树表示方法为：层次表法式)
+    PTree tree; 
+    int length;
+    printf("\n\n");
+
+    InitTree(&tree);        //初始化    
+    length = tree.n;
+
+    PTNode node;
+    PTNode parent;
+    for(int i=0; i < length; i++){
+        node = tree.Nodes[i];       //此时的节点
+        if(node.parent != -1){
+            parent = tree.Nodes[node.parent];   //此时节点的双亲节点
+            printf( "\n节点 %d 的双亲节点为： %d \n", node.data, parent.data);
+        } else{
+            printf(" \n节点 %d 为根节点, 没有双亲节点\n", node.data);
+        }
+    }
+
+    return 0;
+}
---
+++ 数的存储形式/孩子表示法/child.c
@@ -0,0 +1,118 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define MAX_TREE_SIZE 15
+#define ERROR 0
+#define OK 1
+#define FALSE 0
+#define TRUE 1
+
+typedef int Status;
+typedef int ElemType;
+
+typedef struct NodePtr{     //孩子节点
+    int cIndex;  //孩子位置
+    struct NodePtr *SubSib; //孩子节点的下一个兄弟节点
+} Node,*NodePtr;
+
+typedef struct          //双亲节点
+{   
+    ElemType data;      //节点存储的元素数据
+    NodePtr child;      //第一个孩子节点（从左往右）
+} CNode;
+
+typedef struct      //树
+{
+    CNode Nodes[MAX_TREE_SIZE];
+    int r;  //根节点位置
+    int n;  //当前树的节点数目
+} CTree;
+
+/* 测试用的树（层次表示法）: 5 1 3 4 8 9 7 6 2 10
+                5
+        1        3         4
+      8   9        7     6
+            2          10   
+ */
+void InitCTree(CTree *tree){    //初始化
+    ElemType data[] = { 5, 1, 3, 4, 8, 9, 7, 6, 2, 10};
+    int length = sizeof(data) / sizeof(data[0]);
+
+    for(int i = 0; i < length; i++){
+        tree->Nodes[i].data = data[i];
+    }
+
+    tree->n = length;
+    tree->r = 0;
+
+    Node (*child)[length-1] = (NodePtr)malloc( length * sizeof(Node));    //数组指针: 总共需要 存储孩子节点的位置的 节点（除了根节点即可）
+    for(int i=0; i < length-1; i++){
+        child[i]->cIndex = i+1;  //储存所有孩子节点的位置（除根节点）
+    }    
+
+    //根节点
+    tree->Nodes[0].child = child[0];
+    child[0]->SubSib = child[1];
+    child[1]->SubSib = child[2];
+    child[2]->SubSib = NULL;
+    
+    //节点元素 1 的节点
+    tree->Nodes[1].child = child[3];
+    child[3]->SubSib = child[4];
+    child[4]->SubSib = NULL;
+
+    //节点元素 3 的节点
+    tree->Nodes[2].child = child[5];
+    child[5]->SubSib = NULL;
+
+    //节点元素 4 的节点
+    tree->Nodes[3].child = child[6];
+    child[6]->SubSib = NULL;
+
+    //节点元素 9 的节点
+    tree->Nodes[5].child = child[7];
+    child[7]->SubSib = NULL;
+
+    //节点元素 6 的节点
+    tree->Nodes[7].child = child[8];
+    child[8]->SubSib = NULL;
+
+    //叶节点（即没有孩子的节点）: 8 7 2 10
+    tree->Nodes[4].child = tree->Nodes[6].child = tree->Nodes[8].child = tree->Nodes[9].child = NULL;
+}
+
+int main(void){
+    CTree tree;
+    int length;
+
+    printf("\n\n");
+    InitCTree(&tree);
+    length = tree.n;
+
+    printf(" 储存的元素: ");
+    for( int i = 0; i < length; i++){
+        printf(" %d ", tree.Nodes[i].data);
+    }
+
+
+    int i = 0;
+    int n = tree.n;
+    NodePtr child;
+    while( i < n ){
+        printf("\n\n 节点元素 %d 的孩子节点: ", tree.Nodes[i].data);
+        child = tree.Nodes[i].child;
+        if(child != NULL){
+           while(child != NULL){
+                printf(" %d ", tree.Nodes[child->cIndex].data);
+                child = child->SubSib;
+            }
+        } else{
+            printf(" 此节点无孩子节点");
+        }
+        i++;
+    }
+
+
+    printf("\n\n");
+    return 0;
+}
